0559110e3c382e31d86e12b387a5714a80df8f1b
Muhammad-Usman036 <musmanmsadiq036@gmail.com>
2024-10-09 22:38:38 +0500

    pages and routes created

---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+      prj
+    </div>
+  )
+}
+
+export default Projects
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      s up
+    </div>
+  )
+}
+
+export default Signup
