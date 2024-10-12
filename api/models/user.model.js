4b452722882f487c386849e0bcaea03397f4c2ff
Muhammad-Usman036 <musmanmsadiq036@gmail.com>
2024-10-12 17:56:06 +0500

    create User model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+//   this is beacause of to know about user's creation time and updation time
+  { timestamps: true }
+);
+
+const User = mongoose.mmodel('User',userSchema);
+export default User;
