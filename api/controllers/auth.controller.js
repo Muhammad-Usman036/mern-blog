4cdf91fe9ea7b91aa9a140d3cd75b5022491cc70
Muhammad-Usman036 <musmanmsadiq036@gmail.com>
2024-10-13 00:12:23 +0500

    create signup api rooute

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+export const signup = async (req, res) => {
+  // console.log(req.body);
+  // res.send(req.body);
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username == "" ||
+    email == "" ||
+    password == ""
+  ) {
+    return res.status(400).json({ message: "All  filed are required" });
+  }
+//   
+const hashPassword = bcryptjs.hashSync(password,10);
+// 
+  const newUser = new User({ username, email, password:hashPassword,});
+  try {
+    await newUser.save();
+    res.json({ message: "Signup Successfully" });
+  } catch (error) {
+    res.status(500).json({message:error.message});
+  }
+};
--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
+import userAuthRoutes from './routes/auth.route.js';
+const app = express();
+app.use(express.json()); 
 // const mongoose = require('mongoose');
 dotenv.config();
 // console.log('MongoDB connection string:', process.env.MONGO);
@@ -14,8 +17,10 @@ mongoose
     console.log("database is connected");
   }).catch((err)=>console.log(err));
 
-const app = express();
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
+// routes
 app.use('/api/user',userRoutes);
+app.use('/api/auth',userAuthRoutes);
