6a23a5b9c61fa540a0117aab1201f5b63c119814
Muhammad-Usman036 <musmanmsadiq036@gmail.com>
2024-10-14 15:43:32 +0500

    add signup functionality for the signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react"; // Import from flowbite-react
+import React, { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react"; // Import from flowbite-react
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage,setErrorMessage] = useState(null);
+const [loading,setLoading] = useState(false);
+// 
+const navigate = useNavigate();
+  const handleChange = (e)=>{
+    setFormData({...formData ,[e.target.id]: e.target.value.trim()});
+    
+  }
+  // 
+  const handleSubmit = async (e)=>{
+   e.preventDefault ();
+   if (!formData.username || !formData.password || !formData.email ) {
+     return setErrorMessage("Please enter all required fields");
+    }
+    
+   try {setLoading(true);
+    setErrorMessage(null);
+    const res = await fetch('/api/auth/signup',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+
+      },
+      body:JSON.stringify(formData),
+    })
+    const data = await res.json();
+   console.log(data);
+    if (data.succes === false) {
+      console.log('wrong');
+      return setErrorMessage(data.message);
+    }
+    setLoading(false);
+    if (res.ok) {
+      navigate('/signin');
+      
+    }
+   } catch (error) {
+    setErrorMessage(data.message);
+    setLoading(false);
+   }
+  }
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -18,27 +60,36 @@ const Signup = () => {
           </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form onSubmit={handleSubmit}  className="flex flex-col gap-4 ">
             <div className="">
               <Label value="Your Username" />
-              <TextInput type="text" placeholder="username" id="usename" />
+              <TextInput onChange={handleChange} type="text" placeholder="username" id="username" />
             </div>{" "}
             <div className="">
               <Label value="Your email" />
-              <TextInput type="text" placeholder="name@company.com" id="email" />
+              <TextInput onChange={handleChange} type="email" placeholder="name@company.com" id="email" />
             </div>
             <div className="">
               <Label value="Your Password" />
-              <TextInput type="text" placeholder="password" id="password" />
+              <TextInput onChange={handleChange} type="password" placeholder="password" id="password" />
             </div>{" "}
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {loading ? (<>
+               <Spinner size="sm" />
+               <span className="pl-3">Loading...</span>
+             </> ) : 'Sign Up'}
             </Button>
           </form>
           <div className=" flex gap-2 text-sm mt-5">
             <span>Have an account ?</span>
             <Link className="text-blue-500" to="/signin">Sign In</Link>
           </div>
+          {errorMessage && (
+            // <div className="text-red-500 text-sm mt-2">{errorMessage}</div>
+            <Alert className="mt-5 "color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {  // Use 'server' instead of 'serve'
+    proxy: {  // Use 'proxy' instead of 'Proxy'
+      '/api': {
+        target: 'http://localhost:3000',  // Your backend server URL
+        secure: false,  // Use a boolean, not a string for 'secure'
+        changeOrigin: true,  // This ensures the host header matches the target
+      },
+    },
+  },
   plugins: [react()],
+
 })
